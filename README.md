dab4c69cd3f86a17af15d22808814ed7920fe82b
Chris Gradwohl <christophergradwohl@gmail.com>
2017-06-06 15:08:16 -0700

    rendered Header, updated json store, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 ## Grid Report
 a business report application using a responsive data grid.
 
-https://cgradwohl.github.io/grid-report/
+## GitHub Pages
+the app is live: https://cgradwohl.github.io/grid-report/
+
+#### EVENT MEDIATOR
+This Project uses a version of the publish/subscribe and mediator pattern.
+
+To learn more about about these patterns see:
+
+(https://addyosmani.com/resources/essentialjsdesignpatterns/book/#mediatorpatternjavascript)
+
+(https://github.com/addyosmani/pubsubz/blob/master/pubsubz.js)
--- js/reportModule.js
+++ js/reportModule.js
@@ -1,8 +1,6 @@
-/**
-*
-* EVENT AGGREGATION OBJECT
-* based on the publish/subscribe and mediator pattern (https://addyosmani.com/resources/essentialjsdesignpatterns/book/#mediatorpatternjavascript)(https://github.com/addyosmani/pubsubz/blob/master/pubsubz.js)
-*/
+/***    *   *   *   *   *   *  ***/
+/***    @name EVENT MEDIATOR   ***/
+/***    *   *   *   *   *   *  ***/
 const events = {
     events: {},
 
@@ -33,10 +31,9 @@ const events = {
 
 
 
-/**
-*
-* SELECTOR MODULE
-*/
+/***    *   *   *   *   *   *   ***/
+/***    @name SELECTOR MODULE   ***/
+/***    *   *   *   *   *   *   ***/
 (function() {
 
 
@@ -51,60 +48,77 @@ const events = {
 
 
 
-/**
-*
-* GRID MODULE
-*/
+/***    *   *   *   *   *   *   ***/
+/***    @name GRID MODULE       ***/
+/***    *   *   *   *   *   *   ***/
 (function() {
-    let reportArray   = [];
-    let reportColumns = [];
-
 
     // cacheDOM
-
+    let $grid = $('#grid-module');
+    let header_template = $('#grid-header').html();
+    let body_template = $('#grid-body').html();
 
     // bind events
     /*events.on('selectorChange', show);
-    events.on('columnChange', updateGrid);*/
+    events.on('columnChange', updateGrid);
+    events.on('gridHeader', renderHeader);
+    events.on('gridBody', renderBody);*/
 
-    init();
-
-    function init() {
-        // gets JSON and gets it ready to put into the grid template
-        getGridData();
-
-        // render();
-    }
-
+    (function init() {
+        getGridData().then(data => render(data));
+    })();
 
     function getGridData() {
-
-        $.ajax({
-            url: 'data/data.json',
-            dataType: 'json',
-            success: function(response) {
-                reportArray   = response;
-                reportColumns = Object.keys(reportArray[0]);
-
-                events.emit('columnNames', reportColumns);
-            }
+        return new Promise( (resolve, reject) => {
+            $.ajax({
+                url: 'data/data.json',
+                dataType: 'json',
+                success: (response) => resolve(response),
+                error: (err) => reject(err)
+            });
         });
     }
-    // @TODO: render() - renders the grid templates from json data
 
-    // @TODO: show(bool) - if(bool) showMoreStuff else showLessStuff
+    function render(data) {
 
-    // @TODO: updateGrid(colNames) - renders grid based on colNames
+        let headers        = Object.keys(data[0]);
+        let tooManyHeaders = Object.keys(data[0]).length >= 5;
+        let map            = ['','five', 'four', 'three', 'two', 'one'];
+        let index          = 0;
+        let names          = [];
+
+        if( tooManyHeaders ) {
+            names = headers.slice(0,5);
+            index = 5;
+        } else {
+            names = data;
+            index = data.length;
+        }
+
+        // renders Grid Header
+        names.forEach(name => {
+            $grid.append(Mustache.render(header_template,
+                {name: name.toUpperCase(), size: map[index]}
+            ));
+        });
+
+        // filter out selected keys and send values to renderBody()
+        // renders Grid Body
+
+
+    }
+
+    // @TODO: show(bool) - if(bool) render more else render less,
+
+    // @TODO: updateGrid(colNames) - renders grid based on colNames, may be able to only need GridModule.render()
 
 
 })();
 
 
-
-/**
-*
-* DROPDOWN MODULE
-*/
+/***    *   *   *   *   *   *   ***/
+/***    @name DROPDOWN MODULE   ***/
+/***    *   *   *   *   *   *   ***/
 (function() {
 
 
@@ -112,18 +126,24 @@ const events = {
 
 
     // bind events
-    events.on('columnNames', setDropdown);
+    // events.on('columnNames', render);
+    // OR
+    // events.on('columnNames', setDropdown);
+
+    events.on('gridHeader', render);
 
 
     function setDropdown(columnNames) {
         // use columnNames to render the dropdown-content
-        console.log(columnNames);
+        // console.log(columnNames);
 
         // render()
     }
 
 
-    // @TODO: render()
+    function render(data) {
+        // console.log(data);
+    }
 
     // @TODO: applyColumnChange() - events.emit('columnChange', newColumnNames)
 })();
