177b40307e354789c49b16c99db02df9f847a5e5
Chris Gradwohl <christophergradwohl@gmail.com>
2017-06-08 01:38:25 -0700

    added sorting functionality and html string

--- js/reportModule.js
+++ js/reportModule.js
@@ -40,9 +40,9 @@ const events = {
 (function() {
 
     let map                  = ['','five', 'four', 'three', 'two', 'one'];
-    let header_template      = '<div class ="header col {{size}}"><strong>{{name}}</strong></div>';
-    let body_template        = '<div class ="body col {{col.size}}">{{col.value}}<div class="hideMe"></div></div>';
-    let nested_body_template = '<div class ="body col {{col.size}}"><div>{{col.value1}}</div><div class="hideMe">{{col.value2}}</div></div>';
+    let header_template      = '<div class="header col {{size}} {{col_id}}"' + 'onclick="events.emit(' + '&#39;columnSort&#39;' + ', {{col_id}})"' + '><strong>{{name}}</strong></div>';
+    let body_template        = '<div class="body col {{col.size}} {{col._id}}">{{col.value}}<div class="hideMe"></div></div>';
+    let nested_body_template = '<div class="body col {{col.size}} {{col._id}}"><div>{{col.value1}}</div><div class="hideMe">{{col.value2}}</div></div>';
 
     // cacheDOM
     let $grid = $('#grid-module');
@@ -50,6 +50,21 @@ const events = {
     // subscribe to events
     events.on('selectorChange', show);
     events.on('columnChange', updateGrid);
+    events.on('columnSort', sort);
+
+    function sort(id) {
+        let col = [...document.getElementsByClassName(id)];
+        col.shift();
+        console.log(col);
+        col.forEach(item => {
+            $(item).html('dude' + '<div class=' + 'hideMe' + ' style=' + '&quot;&quot;' + '></div>');
+        })
+
+        console.log(col);
+
+        // $("#regTitle").html("Hello World");
+
+    }
 
     // saves ajax success response as a resolved promise
     let gridData = new Promise( (resolve, reject) => {
@@ -77,20 +92,25 @@ const events = {
 
         events.emit('renderDropdown', headers);
 
-        // renders Grid Header
+        // renders Grid Header, id's header columns
+        let i = 1;
         names.map(name => {
             $grid.append(Mustache.render(header_template,
-                {name: name.toUpperCase(), size: map[size]}
+                {name: name.toUpperCase(), size: map[size], col_id: i}
             ));
+            i ++;
         });
 
-        // renders Grid Body
+        // renders Grid Body, id's body columns
+        let j = 1;
         data.map(obj => {
             for (let key in obj) {
                 if( names.includes(key) ) {
                     $grid.append(Mustache.render(body_template,
-                        {col:{value: obj[key], size: map[size]}}
+                        {col:{value: obj[key], size: map[size], _id: j}}
                     ));
+                    if( j===5 ) j=0;
+                    j ++;
                 }
             }
         });
@@ -112,26 +132,33 @@ const events = {
             // uses selected index to map to header names
             selected.map(index => names.push(headers[index]) );
 
-            // renders Grid Header
+            // renders Grid Header, id's header columns
+            let i = 1;
             names.map(name => {
                 $grid.append(Mustache.render(header_template,
-                    {name: name.toUpperCase(), size: map[names.length]}
+                    {name: name.toUpperCase(), size: map[names.length], col_id: i}
                 ));
+                i ++;
             });
 
-            // renders Grid Body
+            // renders Grid Body, id's body columns
+            let j = 1;
             data.map(obj => {
                 for (let key in obj) {
                     if( names.includes(key) ){
                         if( typeof(obj[key])==='object' ){
                             // adds multi valued keys to the nested template
                             $grid.append(Mustache.render(nested_body_template,
-                                {col:{value1: obj[key][0], value2: obj[key][1], size: map[names.length]}}
+                                {col:{value1: obj[key][0], value2: obj[key][1], size: map[names.length], _id: j}}
                             ));
+                            if( j===names.length ) j = 0;
+                            j ++;
                         }else {
                             $grid.append(Mustache.render(body_template,
-                                {col:{value: obj[key], size: map[names.length]}}
+                                {col:{value: obj[key], size: map[names.length], _id: j}}
                             ));
+                            if( j===names.length ) j = 0;
+                            j ++;
                         }
                     }
                 }
@@ -160,8 +187,8 @@ const events = {
 (function() {
 
     // cacheDOM
-    let $dropdown = $('#dropdown-module');
-    let checkbox_template = '<input class="fields" type="checkbox" name="" value="{{value}}">{{name}}<br>';
+    let $dropdown         = $('#dropdown-module');
+    let checkbox_template = '<input class ="fields" type ="checkbox" name ="" value ="{{value}}">{{name}}<br>';
 
     // subscribe to events
     events.on('renderDropdown', init);
@@ -187,7 +214,7 @@ const events = {
             if(field['checked']) numbers.push(field['value']);
         });
 
-        if( numbers.length>5 ) alert("PLEASE SELECT ONLY FIVE FIELDS!")
+        if( numbers.length>5 ) alert("PLEASE SELECT ONLY FIVE FIELDS!");
 
         else events.emit('columnChange', numbers);
     }
